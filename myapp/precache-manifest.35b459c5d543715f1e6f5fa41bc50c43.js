f1a9766f29251ef8fb40459ec407f85ab5892591
Khari Nguyen <kharibloger@gmail.com>
2020-04-04 21:24:49 +0700

    build

---
+++ myapp/precache-manifest.35b459c5d543715f1e6f5fa41bc50c43.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9356bac95af26869fb82253218a5105c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c6e1bfd611633a0ca0e5",
+    "url": "/static/css/2.84c661cf.chunk.css"
+  },
+  {
+    "revision": "c2f65f3bbc2970eb833c",
+    "url": "/static/css/main.adc32810.chunk.css"
+  },
+  {
+    "revision": "c6e1bfd611633a0ca0e5",
+    "url": "/static/js/2.f64f8505.chunk.js"
+  },
+  {
+    "revision": "c2f65f3bbc2970eb833c",
+    "url": "/static/js/main.fdcd86eb.chunk.js"
+  },
+  {
+    "revision": "ba78efeb6a6cf079e0e4",
+    "url": "/static/js/runtime-main.39352176.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
